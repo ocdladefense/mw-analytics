e8dd206ab7da81a29a188fe945f76c9c13fc1897
Jose Bernal <jbernal.web.dev@gmail.com>
2023-04-26 21:08:12 +0000

    First commit.

---
+++ googleAnalytics.php
@@ -0,0 +1,73 @@
+<?php
+if ( !defined( 'MEDIAWIKI' ) ) {
+	die( 'This file is a MediaWiki extension, it is not a valid entry point' );
+}
+
+$wgExtensionCredits['other'][] = array(
+	'path'           => __FILE__,
+	'name'           => 'Google Analytics Integration',
+	'version'        => '2.0.2',
+	'author'         => 'Tim Laqua',
+	'descriptionmsg' => 'googleanalytics-desc',
+	'url'            => 'https://www.mediawiki.org/wiki/Extension:Google_Analytics_Integration',
+);
+
+$wgExtensionMessagesFiles['googleAnalytics'] = dirname(__FILE__) . '/googleAnalytics.i18n.php';
+
+$wgHooks['SkinAfterBottomScripts'][]  = 'efGoogleAnalyticsHookText';
+$wgHooks['ParserAfterTidy'][] = 'efGoogleAnalyticsASAC';
+
+
+$wgGoogleAnalyticsAccount = "UA-32154784-1";
+$wgGoogleAnalyticsAddASAC = false;
+$wgGoogleAnalyticsIgnoreSysops = true;
+$wgGoogleAnalyticsIgnoreBots = true;
+
+function efGoogleAnalyticsASAC( &$parser, &$text ) {
+	global $wgOut, $wgGoogleAnalyticsAccount, $wgGoogleAnalyticsAddASAC;
+
+	if( !empty($wgGoogleAnalyticsAccount) && $wgGoogleAnalyticsAddASAC ) {
+		$wgOut->addScript('<script type="text/javascript">window.google_analytics_uacct = "' . $wgGoogleAnalyticsAccount . '";</script>');
+	}
+
+	return true;
+}
+
+function efGoogleAnalyticsHookText( $skin, &$text='' ) {
+	$text .= efAddGoogleAnalytics();
+	return true;
+}
+
+function efAddGoogleAnalytics() {
+	global $wgGoogleAnalyticsAccount, $wgGoogleAnalyticsIgnoreSysops, $wgGoogleAnalyticsIgnoreBots, $wgUser;
+	if ( $wgUser->isAllowed( 'bot' ) && $wgGoogleAnalyticsIgnoreBots ) {
+		return "\n<!-- Google Analytics tracking is disabled for bots -->";
+	}
+	
+	$groups = $wgUser->getEffectiveGroups();       
+
+	if ( in_array('ignoreanalytics', $groups ) && $wgGoogleAnalyticsIgnoreSysops ) {
+		return "\n<!-- Google Analytics tracking is disabled for this user -->";
+	}
+
+	if ( $wgGoogleAnalyticsAccount === '' ) {
+		return "\n<!-- Set \$wgGoogleAnalyticsAccount to your account # provided by Google Analytics. -->";
+	}
+
+	return <<<HTML
+<script type="text/javascript">
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-32154784-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+</script>
+HTML;
+}
+
+///Alias for efAddGoogleAnalytics - backwards compatibility.
+function addGoogleAnalytics() { return efAddGoogleAnalytics(); }
